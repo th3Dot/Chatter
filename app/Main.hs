d837244a6bfa87c2fe45a8afff5938975b8e9732
Martin Kalenda <chaosikcz@gmail.com>
2018-03-11 17:46:24 +0100

    Simple socket

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
 import Lib
+import Network.Socket
+
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+    conn <- accept sock     -- accept a connection and handle it
+    runConn conn            -- run our server's logic
+    mainLoop sock           -- repeat
+
+runConn :: (Socket, SockAddr) -> IO ()
+runConn (sock, _) = do
+    send sock "Hello!\n"
+    close sock
 
 main :: IO ()
-main = someFunc
+main = do
+    sock <- socket AF_INET Stream 0    -- create socket
+    setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
+    bind sock (SockAddrInet 4243 iNADDR_ANY)   -- listen on TCP port 4242.
+    listen sock 2                              -- set a max of 2 queued connections
+    mainLoop sock
+    
